733eb8ba0625f581187d30083c6439bf08931282
forest-shadow <ivan.arevkov@gmail.com>
2023-06-07 19:31:13 +0200

    feat(App) added app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-const App = () => {
-  return (
-    <div>
-      News
-    </div>
-  );
-}
+import type { FC } from 'react';
+import Box from '@mui/material/Box';
+import { AppHeader } from 'components/layout';
+import { PostPage, CommentAsideView } from './views';
+
+const App: FC = () => (
+  <Box display="flex" flexDirection="column" minHeight="100vh">
+    <AppHeader />
+
+    <Box display="flex" flexGrow="1">
+      <Box flexBasis="80%" bgcolor="#4dabf5" padding="2rem 4rem">
+        <PostPage />
+      </Box>
+
+      <Box flexBasis="30%" bgcolor="#2196f3" padding="2rem">
+        <CommentAsideView />
+      </Box>
+    </Box>
+  </Box>
+);
 
 export default App;
