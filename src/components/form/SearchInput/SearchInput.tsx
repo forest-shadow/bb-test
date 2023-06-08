0e70887e552bb5ed0df3867849de4cfed112c618
forest-shadow <ivan.arevkov@gmail.com>
2023-06-08 23:41:24 +0200

    fix(SearchInput) adjusted appearance and fixed props interface

--- src/components/form/SearchInput/SearchInput.tsx
+++ src/components/form/SearchInput/SearchInput.tsx
@@ -1,17 +1,24 @@
-import type { FC } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
+import type { FC } from 'react';
+import type { TextInputChangeEvent } from 'types/DOM.types';
 import {
   Search,
   SearchIconWrapper,
   StyledInputBase,
 } from './SearchInput.styles';
 
-export const SearchInput: FC = () => (
+interface SearchInputProps {
+  value: string;
+  handleInput: (e: TextInputChangeEvent) => void;
+}
+export const SearchInput: FC<SearchInputProps> = ({ value, handleInput }) => (
   <Search>
     <SearchIconWrapper>
       <SearchIcon />
     </SearchIconWrapper>
     <StyledInputBase
+      value={value}
+      onChange={(e) => handleInput(e)}
       placeholder="Search…"
       inputProps={{ 'aria-label': 'search' }}
     />
