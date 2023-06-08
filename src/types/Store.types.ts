9b5270940e6ff77d487360c547bfc8a92e3fecd3
forest-shadow <ivan.arevkov@gmail.com>
2023-06-08 19:48:17 +0200

    refactor(Store.types) extracted AsyncState interface

--- src/types/Store.types.ts
+++ src/types/Store.types.ts
@@ -2,3 +2,9 @@ import type { AnyAction, ThunkDispatch } from '@reduxjs/toolkit';
 import type { IAppState } from 'store';
 
 export type TThunkDispatch = ThunkDispatch<IAppState, unknown, AnyAction>;
+
+export interface AsyncState<T> {
+  isLoading: boolean;
+  error: string | null;
+  data: T | null;
+}
