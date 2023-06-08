d4544e6d15d0d496f5ef7c08ab25defd833ac114
forest-shadow <ivan.arevkov@gmail.com>
2023-06-08 17:54:35 +0200

    feat(Posts) handled current post selection
    * added action for current post selection
    * updated eslint rules

--- src/views/Posts/Post.tsx
+++ src/views/Posts/Post.tsx
@@ -9,11 +9,17 @@ import type { IPost } from 'types/Post.types';
 interface PostProps {
   post: IPost;
   isSelected: boolean;
+  onClickHandler: (post: IPost) => void;
 }
-export const Post: FC<PostProps> = ({ post, isSelected }) => {
+export const Post: FC<PostProps> = ({ post, isSelected, onClickHandler }) => {
   const { title, body } = post;
   return (
-    <Card sx={{ border: isSelected ? `3px solid ${blue[800]}` : 'none' }}>
+    <Card
+      sx={{
+        border: isSelected ? `3px solid ${blue[800]}` : '3px solid transparent',
+      }}
+      onClick={() => onClickHandler(post)}
+    >
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {title}
--- src/views/Posts/PostGrid.tsx
+++ src/views/Posts/PostGrid.tsx
@@ -1,7 +1,11 @@
+import { useCallback } from 'react';
 import type { FC } from 'react';
 import Box from '@mui/material/Box';
+import { useDispatch } from 'react-redux';
 
+import { postActions } from 'store/postSlice';
 import type { PostState } from 'store/postSlice';
+import type { IPost } from 'types/Post.types';
 import { Post } from './Post';
 import { PostGridSkeleton } from './PostGridSkeleton';
 
@@ -10,10 +14,18 @@ interface PostGridProps {
 }
 export const PostGrid: FC<PostGridProps> = ({ postState }) => {
   const { data: posts, isLoading, selected: selectedPost } = postState;
+  const dispatch = useDispatch();
+  const setCurrentPost = useCallback((post: IPost) => {
+    dispatch(postActions.setCurrentPost(post));
+  }, []);
   return (
     <Box display="grid" gridTemplateColumns="repeat(3, 1fr)" gap="2rem">
       {posts?.map((post) => (
-        <Post post={post} isSelected={post.id === selectedPost?.id} />
+        <Post
+          post={post}
+          isSelected={post.id === selectedPost?.id}
+          onClickHandler={setCurrentPost}
+        />
       ))}
 
       {isLoading && <PostGridSkeleton />}
