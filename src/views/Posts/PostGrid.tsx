420e4458bb1c8b7516ea0e67ca32d5e7228b1e56
forest-shadow <ivan.arevkov@gmail.com>
2023-06-08 17:29:13 +0200

    feat(Post) connected to api

--- src/views/Posts/PostGrid.tsx
+++ src/views/Posts/PostGrid.tsx
@@ -1,19 +1,22 @@
 import type { FC } from 'react';
 import Box from '@mui/material/Box';
+
+import type { PostState } from 'store/postSlice';
 import { Post } from './Post';
+import { PostGridSkeleton } from './PostGridSkeleton';
 
-export const PostGrid: FC = () => (
-  <Box display="grid" gridTemplateColumns="repeat(3, 1fr)" gap="2rem">
-    <Post />
-    <Post />
-    <Post />
+interface PostGridProps {
+  postState: PostState;
+}
+export const PostGrid: FC<PostGridProps> = ({ postState }) => {
+  const { data: posts, isLoading, selected: selectedPost } = postState;
+  return (
+    <Box display="grid" gridTemplateColumns="repeat(3, 1fr)" gap="2rem">
+      {posts?.map((post) => (
+        <Post post={post} isSelected={post.id === selectedPost?.id} />
+      ))}
 
-    <Post />
-    <Post />
-    <Post />
-
-    <Post />
-    <Post />
-    <Post />
-  </Box>
-);
+      {isLoading && <PostGridSkeleton />}
+    </Box>
+  );
+};
