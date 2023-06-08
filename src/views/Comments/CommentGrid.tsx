47cee0fb0c9450c993c488bc1649709983d6ce82
forest-shadow <ivan.arevkov@gmail.com>
2023-06-08 20:03:23 +0200

    feat(Comments) displayed selected post comments

--- src/views/Comments/CommentGrid.tsx
+++ src/views/Comments/CommentGrid.tsx
@@ -1,11 +1,36 @@
+import { useEffect } from 'react';
 import type { FC } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Box from '@mui/material/Box';
-import { Comment } from './Comment';
 
-export const CommentGrid: FC = () => (
-  <Box display="flex" flexDirection="column" gap="1rem">
-    <Comment />
-    <Comment />
-    <Comment />
-  </Box>
-);
+import { selectedPostSelector } from 'store/postSlice';
+import {
+  commentsStateSelector,
+  fetchComments,
+  commentActions,
+} from 'store/commentSlice';
+import type { TThunkDispatch } from 'types/Store.types';
+import { Comment } from './Comment';
+import { CommentGridSkeleton } from './CommentGridSkeleton';
+
+export const CommentGrid: FC = () => {
+  const dispatch = useDispatch<TThunkDispatch>();
+  const selectedPost = useSelector(selectedPostSelector);
+  const { data: comments, isLoading } = useSelector(commentsStateSelector);
+  useEffect(() => {
+    if (selectedPost) {
+      dispatch(commentActions.resetComments());
+      dispatch(fetchComments(selectedPost.id));
+    }
+  }, [selectedPost?.id]);
+  return (
+    <Box display="flex" flexDirection="column" gap="1rem">
+      {(isLoading || !comments) && <CommentGridSkeleton />}
+
+      {comments?.length &&
+        comments.map((comment) => (
+          <Comment comment={comment} key={comment.id} />
+        ))}
+    </Box>
+  );
+};
