321165317bc72860468c638a521e3e1953b5dfc3
forest-shadow <ivan.arevkov@gmail.com>
2023-06-08 23:41:24 +0200

    fix(SearchInput) adjusted appearance and fixed props interface

--- src/components/form/SearchInput/SearchInput.styles.tsx
+++ src/components/form/SearchInput/SearchInput.styles.tsx
@@ -32,12 +32,6 @@ export const StyledInputBase = styled(InputBase)(({ theme }) => ({
     padding: theme.spacing(1, 1, 1, 0),
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '300px',
-      '&:focus': {
-        width: '370px',
-      },
-    },
+    width: '300px',
   },
 }));
