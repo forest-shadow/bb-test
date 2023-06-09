f3c85da98badb9ef20b62376a9d77146901a149b
forest-shadow <ivan.arevkov@gmail.com>
2023-06-09 12:34:06 +0200

    feat(Comments) added tagging ability
    * comments loading logic extracted to current post handler of PostGrid

--- src/views/Comments/CommentGrid.tsx
+++ src/views/Comments/CommentGrid.tsx
@@ -1,35 +1,43 @@
-import { useEffect } from 'react';
+import { useCallback } from 'react';
 import type { FC } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Box from '@mui/material/Box';
 
-import { selectedPostSelector } from 'store/postSlice';
-import {
-  commentsStateSelector,
-  fetchComments,
-  commentActions,
-} from 'store/commentSlice';
+import { commentsStateSelector, commentActions } from 'store/commentSlice';
 import type { TThunkDispatch } from 'types/Store.types';
+import type { IComment } from 'types/Comment.types';
+import type { ITag } from 'components/form/TagInput/TagInput';
 import { Comment } from './Comment';
 import { CommentGridSkeleton } from './CommentGridSkeleton';
 
+const commentTags: ITag[] = [
+  { title: 'Tag #1', slug: 'tag-1' },
+  { title: 'Tag #2', slug: 'tag-2' },
+  { title: 'Tag #3', slug: 'tag-3' },
+  { title: 'Tag #4', slug: 'tag-4' },
+];
+
 export const CommentGrid: FC = () => {
   const dispatch = useDispatch<TThunkDispatch>();
-  const selectedPost = useSelector(selectedPostSelector);
   const { data: comments, isLoading } = useSelector(commentsStateSelector);
-  useEffect(() => {
-    if (selectedPost) {
-      dispatch(commentActions.resetComments());
-      dispatch(fetchComments(selectedPost.id));
-    }
-  }, [selectedPost?.id]);
+
+  const handleTagSelection = useCallback((comment: IComment, tags: ITag[]) => {
+    dispatch(commentActions.setCommentTags({ comment, tags }));
+  }, []);
+
   return (
     <Box display="flex" flexDirection="column" gap="1rem">
-      {(isLoading || !comments) && <CommentGridSkeleton />}
+      {isLoading && <CommentGridSkeleton />}
 
-      {comments?.length &&
+      {!isLoading &&
+        comments?.length > 0 &&
         comments.map((comment) => (
-          <Comment comment={comment} key={comment.id} />
+          <Comment
+            comment={comment}
+            key={comment.id}
+            onTagSelect={handleTagSelection}
+            tags={commentTags}
+          />
         ))}
     </Box>
   );
--- src/views/Posts/PostGrid.tsx
+++ src/views/Posts/PostGrid.tsx
@@ -1,10 +1,12 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import type { FC } from 'react';
 import Box from '@mui/material/Box';
 import { useDispatch } from 'react-redux';
 
 import { postActions } from 'store/postSlice';
 import type { PostState } from 'store/postSlice';
+import { commentActions, fetchComments } from 'store/commentSlice';
+import type { TThunkDispatch } from 'types/Store.types';
 import type { IPost } from 'types/Post.types';
 import { Post } from './Post';
 import { PostGridSkeleton } from './PostGridSkeleton';
@@ -14,10 +16,18 @@ interface PostGridProps {
 }
 export const PostGrid: FC<PostGridProps> = ({ postState }) => {
   const { data: posts, isLoading, selected: selectedPost } = postState;
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<TThunkDispatch>();
+
   const setCurrentPost = useCallback((post: IPost) => {
     dispatch(postActions.setCurrentPost(post));
+    dispatch(commentActions.resetComments());
+    dispatch(fetchComments(post.id));
   }, []);
+
+  useEffect(() => {
+    if (selectedPost) dispatch(fetchComments(selectedPost.id));
+  }, [selectedPost]);
+
   return (
     <Box display="grid" gridTemplateColumns="repeat(3, 1fr)" gap="2rem">
       {posts?.map((post) => (
